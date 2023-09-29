382b19483d6ebafe77c57d7a6a261f1e7cfa1829
Vergiananta <christian.vergi@gmail.com>
2023-09-30 06:40:25 +0700

    callback

--- index.js
+++ index.js
@@ -10,3 +10,43 @@ function person(name, callback) {
 person("egi", function(response) {
     console.log(response)
 })
+
+
+// // case 2:
+
+// // Anggap ini adalah data yang ada di dalam database 
+const data = [1, 2, 3]
+
+function hapusData() {
+    setTimeout(function () {
+        // hapus salah satu data
+        data.pop();
+    }, 500);
+}
+
+function tampilData() {
+    setTimeout(function () {
+        // tampilkan semua data
+        console.log(data)
+    }, 300);
+}
+
+hapusData()
+tampilData()
+
+// penyederhanaan case 2 
+function hapusData(callback) {
+    setTimeout(function () {
+        // remove value dari array data
+        data.pop();
+        // panggil function tampilData
+        callback()
+    }, 500);
+}
+
+hapusData(function tampilData() {
+    setTimeout(function () {
+        // tampilkan value dari array data
+        console.log(data)
+    }, 300);
+})
