5f446c63b393823913c363bfaebc0dddebc94ba4
Vergiananta <christian.vergi@gmail.com>
2023-09-30 06:29:52 +0700

    callback

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const role = "staff"
 
 function person(name, callback) {
-    console.log("Proses karyawan .....")
+    console.log(`Proses data karyawan ${name} .....`)
     setTimeout(()=> {
         callback({name, role})
     },1500)
